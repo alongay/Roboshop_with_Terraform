bab55f04d17257a31481cb795d142a25fe3ccaa1
Kwasi Alongay <alongay@yahoo.com>
2021-11-09 16:37:13 -0500

    create ec2 terraform

--- examples/main.tf
+++ examples/main.tf
@@ -2,19 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "sample" {
-  ami                    = "ami-0dc863062bc04e1de"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id, "sg-0bcf4abee87c39a2d"]
-
-  tags = {
-    Name = "sample"
-  }
-}
-
+#Create Security Group allow_sample
 resource "aws_security_group" "allow_sample" {
   name        = "allow_sample"
-  description = "Allow sample traffic"
+  description = "Allow Sample inbound traffic"
 
   ingress = [
     {
@@ -24,23 +15,16 @@ resource "aws_security_group" "allow_sample" {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
     }
   ]
 
   egress = [
     {
-      description      = "egress"
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
     }
   ]
 
@@ -49,12 +33,13 @@ resource "aws_security_group" "allow_sample" {
   }
 }
 
+# Create EC2 instance
+resource "aws_instance" "sample" {
+  ami                    = "ami-077fb3e62ddf0fa9a"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_sample.id]
 
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-b60"
-    key    = "example/sample/terraform.tfstate"
-    region = "us-east-1"
+  tags = {
+    Name = "sample"
   }
 }
