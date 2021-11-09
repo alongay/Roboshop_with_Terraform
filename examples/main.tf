b4df87787451c50e2f752598d0a82b1acd2bf928
Kwasi Alongay <alongay@yahoo.com>
2021-11-09 17:05:39 -0500

    attribute description required for egress

--- examples/main.tf
+++ examples/main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_sample" {
 
   egress = [
     {
-      description      = ["egress for sample sg"]
+      description      = "egress"
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
