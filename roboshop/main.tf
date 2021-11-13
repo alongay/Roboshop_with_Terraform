0409ae3778f779ee8636e2c34d820b0ee3da5792
Kwasi Alongay <alongay@yahoo.com>
2021-11-13 01:35:49 -0500

    roboshop setup

---
+++ roboshop/main.tf
@@ -0,0 +1,20 @@
+# Create EC2 instance
+resource "aws_instance" "sample" {
+  count                  = length(var.components)
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-049e9bd07e7efb3ad"]
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "^centos*"
+  owners = [973714476881]
+}
+
+variable "components" {
+  default = ["frontend", "mongodb", "catalogue", "cart", "user", "radis", "mysql", "shipping", "rebbitmq","payment"]
+}
