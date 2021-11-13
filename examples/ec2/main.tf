54701d89727fe4bde4d160bfc535407f9c680257
Kwasi Alongay <alongay@yahoo.com>
2021-11-12 23:52:10 -0500

    modules ec2 sg loop using conditions

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "sample" {
   count                  = length(var.name)
   ami                    = "ami-077fb3e62ddf0fa9a"
-  instance_type          = "t3.micro"
+  instance_type          = var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
 
   tags = {
@@ -12,6 +12,7 @@ resource "aws_instance" "sample" {
 
 variable "SGID" {}
 variable "name" {}
+variable "instance_type" {}
 
 #output "public_ip" {
 #  value = aws_instance.sample.private_ip
